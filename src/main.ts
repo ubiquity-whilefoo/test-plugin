f865bd791fb8d4f3fb4d5fdc2fa83691a268008d
whilefoo <whilefoo@users.noreply.github.com>
2024-10-23 11:34:41 +0200

    feat: logging

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { runPlugin } from "./plugin";
 import { createActionsPlugin } from "@ubiquity-os/ubiquity-os-kernel";
+import * as github from "@actions/github";
 
+console.log(github.context.payload.inputs);
 createActionsPlugin(runPlugin).then(console.log).catch(console.error);
