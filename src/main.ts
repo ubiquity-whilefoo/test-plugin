7cce7cb369e1c6bf8462c04ff322efdf4ae2fdba
whilefoo <whilefoo@users.noreply.github.com>
2024-10-23 12:15:39 +0200

    feat: custom kernel public key

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { runPlugin } from "./plugin";
 import { createActionsPlugin } from "@ubiquity-os/ubiquity-os-kernel";
-import * as github from "@actions/github";
 
-console.log(github.context.payload.inputs);
-createActionsPlugin(runPlugin).then(console.log).catch(console.error);
+createActionsPlugin(runPlugin, {
+  kernelPublicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsdKRgFiOZ9xC8B9NGhTh
+fceon3/ypqCzRoJAXnbI+qdgZfgKtWsQILbrLeRtT2W8/NA9rBBfTIIZRfLfSEAS
+T8tYCFEWb5xv67RUoN1Jd2iRrbmdT2/gA5J6CBp/PrygqF2o79IpSEC50qOw2aRD
+Nw4GEonX1NABnPZy+P9WxZFdQotl/s6Cfgh6EDFhmmfU4gpRmHVrG7ztkcYByX6e
+a/B/780Kt6QQDNJ8tuEm+vJ1kihYrz5jt47YTifCRrjPnqD57sa0FCObGSMyVl8k
++sHL5LbY0lhPlI0XZDe6UTnUvrataKd5teR+V6jKzarpKn/513PMUyZwWU3dLiZo
+uwIDAQAB
+-----END PUBLIC KEY-----
+`,
+})
+  .then(console.log)
+  .catch(console.error);
