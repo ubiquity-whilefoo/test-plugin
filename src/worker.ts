d5a0984df5e6a50a57cd2c09d934b80dd06ffb6b
whilefoo <whilefoo@users.noreply.github.com>
2024-10-28 09:22:30 +0100

    feat: upgrade kernel

--- src/worker.ts
+++ src/worker.ts
@@ -1,10 +1,19 @@
 import { runPlugin } from "./plugin";
 import { createPlugin } from "@ubiquity-os/ubiquity-os-kernel";
 
-const app = createPlugin(runPlugin, { name: "test plugin" });
-
-export default {
-  async fetch(req, env, ctx) {
-    return (await app).fetch(req, env, ctx);
-  },
-};
+export default createPlugin(
+  runPlugin,
+  { name: "test plugin" },
+  {
+    kernelPublicKey: `-----BEGIN PUBLIC KEY-----
+  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsdKRgFiOZ9xC8B9NGhTh
+  fceon3/ypqCzRoJAXnbI+qdgZfgKtWsQILbrLeRtT2W8/NA9rBBfTIIZRfLfSEAS
+  T8tYCFEWb5xv67RUoN1Jd2iRrbmdT2/gA5J6CBp/PrygqF2o79IpSEC50qOw2aRD
+  Nw4GEonX1NABnPZy+P9WxZFdQotl/s6Cfgh6EDFhmmfU4gpRmHVrG7ztkcYByX6e
+  a/B/780Kt6QQDNJ8tuEm+vJ1kihYrz5jt47YTifCRrjPnqD57sa0FCObGSMyVl8k
+  +sHL5LbY0lhPlI0XZDe6UTnUvrataKd5teR+V6jKzarpKn/513PMUyZwWU3dLiZo
+  uwIDAQAB
+  -----END PUBLIC KEY-----
+  `,
+  }
+);
